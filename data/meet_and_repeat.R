042cd48d4a003b2a370d21d116b3a72287a6fb26
sviitama <sirja.viitamaki@helsinki.fi>
2019-12-09 23:06:40 +0200

    Exercise 6 finished

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,52 @@
+# Sirja Viitamäki
+# This script creates a modified version of dataset
+# 9.12.2019
+
+# Set working directory
+setwd("C:/Users/sirja/Documents/IODS-project-1")
+#setwd("Z:/Jatko-opinnot/IODS/IODS-project-1")
+
+# Access needed libraries
+library(tidyverse)
+
+# Read in datasets
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", header=TRUE, sep=" ")
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header=TRUE, sep="\t")
+
+names(BPRS)
+names(RATS)
+str(BPRS)
+str(RATS)
+dim(BPRS)
+dim(RATS)
+
+# Convert the categorical variables to factors
+BPRS$treatment <- factor(BPRS$treatment)
+BPRS$subject <- factor(BPRS$subject)
+
+RATS$ID <- factor(RATS$ID)
+RATS$Group <- factor(RATS$Group)
+
+# Convert the data sets to long form and add a week variable to BPRS and a Time variable to RATS
+BPRSL <- BPRS %>% gather(key=weeks, value=bprs, -treatment, -subject)
+BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(weeks, 5, 5)))
+RATSL <- RATS %>% gather(key=WD, value=Weight, -ID, -Group) %>%
+  mutate(Time = as.integer(substr(WD, 3,4))) 
+
+names(BPRS)
+names(BPRSL)
+str(BPRS)
+str(BPRSL)
+summary(BPRS)  
+summary(BPRSL)
+
+names(RATS)
+names(RATSL)
+str(RATS)
+str(RATSL)
+summary(RATS)
+summary(RATSL)
+
+# Save the data
+write.csv(BPRSL, "data/BPRSL.txt")
+write.csv(RATSL, "data/RATSL.txt")
